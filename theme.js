54ba5732e6c0715bf4340c1d0607740865382bc1
Samuel Ige <samuelige.iyanu@gmail.com>
2023-10-24 11:51:17 +0100

    add - stye themes. And image url setup

---
+++ theme.js
@@ -0,0 +1,30 @@
+module.exports = {
+    fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+    },
+    colors: {
+        mainYellow: "#FF9F1C",
+        mainRed : "#FF4040",
+        mainGreen: "#2EC4B6",
+        dark: "#OA1014",
+        gray: "#1B2329",
+        midGray: "#353F4C",
+        lightGray: "#7A8C99",
+        white: "#FFF",
+    },
+    backgroundColor: {
+        darkBlueGray: "#666CA3",
+        yankeesBlue : "#13183F",
+        // frostbiteOrange: "linear-gradient(#F02AA6, #FF6F48)",
+        // blueFrostbite: "linear-gradient(#4851FF, #F02AA6)",
+    },
+    backgroundImage: {
+        frostbiteOrange: "linear-gradient(#F02AA6, #FF6F48)",
+        frostbiteOrangeHover: "linear-gradient(0deg, rgba(255, 255, 255, 0.50) 0%, rgba(255, 255, 255, 0.50) 100%), linear-gradient(180deg, #FF6F48 0%, #F02AA6 100%)",
+        blueFrostbite: "linear-gradient(#4851FF, #F02AA6)",
+        blueFrostbiteHover: "linear-gradient(0deg, rgba(255, 255, 255, 0.50) 0%, rgba(255, 255, 255, 0.50) 100%), linear-gradient(180deg, #4851FF -54.32%, #F02AA6 100%)",
+        whiteGradient: "linear-gradient(180deg, #FFF 0%, #F0F1FF 100%)",
+      },
+    borderColor: {},
+    
+}
