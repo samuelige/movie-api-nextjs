d1776b745a4fe277bb22b9a2522eec0869d053fa
Samuel Ige <samuelige.iyanu@gmail.com>
2023-10-26 01:33:38 +0100

    add README.md file

--- src/_shared/components/CustomBtn.tsx
+++ src/_shared/components/CustomBtn.tsx
@@ -16,7 +16,7 @@ interface CustomBtnProps {
 
 const CustomBtn:FC<CustomBtnProps> = ({image, isNested, isActive, bgColor, brdColor, content, size,activeColor, handleClick}) => {
   return (
-    <button onClick={handleClick} className={` text-lightGray ${isActive ? activeColor : "bg-transparent border-[1px] border-lightGray"} hover:border-mainRed flex flex-row items-center  font-medium rounded-md text-xl ${isNested ? "cursor-not-allowed border-0  text-white" : " pl-2 pr-2 py-2 space-x-2"}`}>
+    <button onClick={handleClick} className={` text-lightGray ${isActive ? activeColor : "bg-transparent "} hover:border-mainRed flex flex-row items-center  font-medium rounded-md text-xl ${isNested ? "cursor-not-allowed border-0  text-white" : " pl-2 pr-2 py-2 space-x-2 border-[1px] border-lightGray"}`}>
         {
             isNested ? (
                 <>
